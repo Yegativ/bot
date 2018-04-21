cf37272e93d4bb2e5c795ca71f7082a8e13e54f3
Yegativ <37016303+Yegativ@users.noreply.github.com>
2018-04-21 16:05:49 +0600

    bot

--- bot.py
+++ bot.py
@@ -1,26 +1,17 @@
 import telebot
 import config
-
+import requests
 bot = telebot.TeleBot(config.token)
-produkt = ("картофель","лук","колбаса","майонез","соленые огурцы","морковь")
-gramm = (50, 20, 100, 15, 30, 30)
-@bot.message_handler(commands = ["olivie"])
-def answer_olivie(message):
-    bot.send_message(message.chat.id, "Введите количество человек: ")
-    olivie(message)
-
-
 @bot.message_handler(content_types = ["text"])
-def olivie(message):
-    print(message)
-    try:
-        people_count = int(message.text)
-        for i in range(0, len(produkt)):
-            bot.send_message(message.chat.id, produkt[i] + " - " + str(gramm[i] * people_count) + "гр.")
-    except:
-        print("Неправильное число")
-        
+def ip(message):
+    data = requests.get("https://api.2ip.ua/geo.json?ip="+str(message.text))
+    print(str(message))
+    bot.send_message(message.chat.id, data.json()["country"])
+    bot.send_message(message.chat.id,  data.json()["city"])
+    bot.send_message(message.chat.id, data.json()["zip_code"])
+
+
+
 if __name__ =='__main__':
     bot.polling(none_stop=True)
-    
     
